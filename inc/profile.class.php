886709848d5fe96312618b5463770cd5d332f85a
tsmr <thetsmr@gmail.com>
2018-08-08 16:28:37 +0200

    Fix deprecated

--- inc/profile.class.php
+++ inc/profile.class.php
@@ -93,11 +93,11 @@ class PluginMydashboardProfile extends CommonDBTM {
       $profileRight = new ProfileRight();
       foreach ($rights as $right => $value) {
          if (countElementsInTable('glpi_profilerights',
-                                  "`profiles_id`='$profiles_id' AND `name`='$right'") && $drop_existing) {
+                                  ["profiles_id" => $profiles_id, "name" => $right]) && $drop_existing) {
             $profileRight->deleteByCriteria(['profiles_id' => $profiles_id, 'name' => $right]);
          }
          if (!countElementsInTable('glpi_profilerights',
-                                   "`profiles_id`='$profiles_id' AND `name`='$right'")) {
+                                   ["profiles_id" => $profiles_id, "name" => $right])) {
             $myright['profiles_id'] = $profiles_id;
             $myright['name']        = $right;
             $myright['rights']      = $value;
@@ -250,7 +250,7 @@ class PluginMydashboardProfile extends CommonDBTM {
       //Add new rights in glpi_profilerights table
       foreach ($profile->getAllRights(true) as $data) {
          if (countElementsInTable("glpi_profilerights",
-                                  "`name` = '" . $data['field'] . "'") == 0) {
+                                  ["name" => $data['field']]) == 0) {
             ProfileRight::addProfileRights([$data['field']]);
          }
       }
