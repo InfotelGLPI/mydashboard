cfdd4a9c3377a310dc05265c3fbec00dad8c694c
keha35 <kevin.hainry@infotel.com>
2018-12-06 15:11:20 +0100

    Update install plugin

--- install/sql/update-1.6.3.sql
+++ install/sql/update-1.6.3.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `glpi_plugin_mydashboard_dashboards` ADD `grid_statesave` LONGTEXT NULL DEFAULT NULL;
-ALTER TABLE `glpi_plugin_mydashboard_config` ADD `levelCat` int(11) NOT NULL DEFAULT '2';
+ALTER TABLE `glpi_plugin_mydashboard_configs` ADD `levelCat` int(11) NOT NULL DEFAULT '2';
