9e8fd54e040aabe698600f8970dafe66f0a308c1
Kevin HAINRY <39367117+keha35@users.noreply.github.com>
2019-03-29 09:10:39 +0100

    Update widgetlist.class.php

--- inc/widgetlist.class.php
+++ inc/widgetlist.class.php
@@ -64,13 +64,7 @@ class PluginMydashboardWidgetlist {
       if (isset($PLUGIN_HOOKS['mydashboard'])) {
          $widgets = (isset($PLUGIN_HOOKS['mydashboard'])?$PLUGIN_HOOKS['mydashboard']:[]);
       }
-      
-      //We add classes from mydashboard
-      $widgets['mydashboard'] = [
-         'PluginMydashboardInfotel',
-         'PluginMydashboardAlert'
-      ];
-      
+            
       //We add classes from mydashboard
       $widgets['mydashboard'] = [
          'PluginMydashboardInfotel',
