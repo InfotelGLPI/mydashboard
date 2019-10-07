a84cf5c1d2e437237d85f547e8538e8d9a959b10
tsmr <thetsmr@gmail.com>
2019-10-07 11:00:40 +0200

    Minor fix

--- inc/helper.class.php
+++ inc/helper.class.php
@@ -166,7 +166,7 @@ class PluginMydashboardHelper {
       $graph = "<div class='bt-row'>";
       $graph .= "<div class='bt-col-md-12 left'>";
       if (isset($params["setup"]) && Session::haveRightsOr("plugin_mydashboard_stockwidget", [CREATE, UPDATE])) {
-         $graph .= "<a target='_blank' href='".$params["setup"]."'><i class=\"far fa-edit fa-1x\"></i></a>";
+         $graph .= "<a target='_blank' href='".$params["setup"]."'><i class=\"fas fa-wrench fa-1x\"></i></a>";
       }
       $graph .= "</div>";
       $graph .= "</div>";
