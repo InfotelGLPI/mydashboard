cb0cc821cde5f7c1d9219f6b6039885148592e0c
tsmr <thetsmr@gmail.com>
2020-03-14 14:38:17 +0100

    Min css fix

--- scripts/mydashboard.js
+++ scripts/mydashboard.js
@@ -44,3 +44,50 @@ var mydashboard = {
         }, delay);
     },
 };
+
+/**
+ *  Load plugin scripts on page start
+ */
+//(function ($) {
+//    $.fn.mydashboard_load_scripts = function () {
+
+//        init();
+
+        // Start the plugin
+//        function init() {
+            //            $(document).ready(function () {
+//            var path = 'plugins/mydashboard/';
+//            var url = window.location.href.replace(/front\/.*/, path);
+//            if (window.location.href.indexOf('plugins') > 0) {
+//                url = window.location.href.replace(/plugins\/.*/, path);
+//            }
+
+            // Send data
+//            $.ajax({
+//                url: url+'ajax/loadscripts.php',
+//                type: "POST",
+//                dataType: "html",
+//                data: 'action=load',
+//                success: function (response, opts) {
+//                    var scripts, scriptsFinder = /<script[^>]*>([\s\S]+?)<\/script>/gi;
+//                    while (scripts = scriptsFinder.exec(response)) {
+//                        eval(scripts[1]);
+//                    }
+//                }
+//            });
+//            if($("#mydashboard_link").length == 0) {
+//                $("#c_preference ul #bookmark_link").before("\
+//                <li id='mydashboard_link'>\
+//                    <a href='#' id='showMydashboardMenuLink'>\
+//                        <i id='mydashboard_icon' class='fas fa-tachometer-alt fa-2x' title='' class='button-icon'></i>\
+//                    </a>\
+//                </li>");
+//            }
+            //            });
+//        }
+
+//        return this;
+//    }
+//}(jQuery));
+
+//$(document).mydashboard_load_scripts();
