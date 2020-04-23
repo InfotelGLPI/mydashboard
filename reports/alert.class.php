0b06866e903fa28942d9b5a70fa3d1a58434a6d1
tsmr <thetsmr@gmail.com>
2020-04-23 15:26:46 +0200

    fix #69

--- reports/alert.class.php
+++ reports/alert.class.php
@@ -2374,7 +2374,7 @@ class PluginMydashboardAlert extends CommonDBTM {
       }
       curl_setopt($ch, CURLOPT_URL, $url);
       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-      curl_setopt($ch, CURLOPT_HEADER, 0);
+      curl_setopt($ch, CURLOPT_HEADER, 1);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
       curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)");
       curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
