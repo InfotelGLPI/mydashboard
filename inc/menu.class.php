212f69571ddc45057cd8cbd99068e5d81d9a4658
ales-infotel <alban.lesellier@infotel.com>
2020-12-17 17:39:18 +0100

    disable cache for dashboard

--- inc/menu.class.php
+++ inc/menu.class.php
@@ -1297,9 +1297,9 @@ class PluginMydashboardMenu extends CommonGLPI {
             $ckey         = 'md_cache_' . md5($widgetclasse->getTable()).Session::getLoginUserID();
             $datas     = $GLPI_CACHE->get($ckey);
             //UNACTIVATE IT FOR DEBUG
-            if (is_array($datas) && count($datas) > 0 && $predefined_grid == 0) {
-               $datajson = $datas;
-            } else {
+//            if (is_array($datas) && count($datas) > 0 && $predefined_grid == 0) {
+//               $datajson = $datas;
+//            } else {
                $widgets = PluginMydashboardWidget::getWidgetList();
 
                foreach ($datagrid as $k => $v) {
@@ -1319,7 +1319,7 @@ class PluginMydashboardMenu extends CommonGLPI {
                if ($predefined_grid == 0) {
                   $GLPI_CACHE->set($ckey, $datajson);
                }
-            }
+//            }
       } else {
          echo "<div class='bt-alert bt-alert-warning' id='warning-alert'>
                 <strong>" . __('Warning', 'mydashboard') . "!</strong>
