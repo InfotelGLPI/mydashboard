d108d61383a5e6713f4ff87f0d6c6695cf6b518e
tsmr <thetsmr@gmail.com>
2022-02-17 13:36:24 +0100

    add helpdesk_menu_entry_icon

--- setup.php
+++ setup.php
@@ -105,6 +105,7 @@ function plugin_init_mydashboard() {
 
             $PLUGIN_HOOKS['menu_toadd']['mydashboard']          = ['tools' => 'PluginMydashboardMenu'];
             $PLUGIN_HOOKS['helpdesk_menu_entry']['mydashboard'] = PLUGIN_MYDASHBOARD_NOTFULL_DIR.'/front/menu.php';
+            $PLUGIN_HOOKS['helpdesk_menu_entry_icon']['mydashboard'] = PluginMydashboardMenu::getIcon();
 
             //            $CFG_GLPI['javascript']['tools']['pluginmydashboardmenu']['PluginMydashboardConfig'] = ['colorpicker'];
 
