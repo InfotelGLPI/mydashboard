a60aa5bf5d9518d17df4e1756a112e23b73e65d7
tsmr <thetsmr@gmail.com>
2022-03-23 08:56:03 +0100

    Prepare 1.8.3

--- setup.php
+++ setup.php
@@ -24,7 +24,7 @@
  --------------------------------------------------------------------------
  */
 
-define('PLUGIN_MYDASHBOARD_VERSION', '1.8.2');
+define('PLUGIN_MYDASHBOARD_VERSION', '1.8.3');
 
 // Init the hooks of the plugins -Needed
 function plugin_init_mydashboard() {
