f6279b3cc8495a6a76c870aeff17b5d1a3f7cb05
tsmr <thetsmr@gmail.com>
2023-03-28 11:37:18 +0200

    Fix replace central

--- scripts/replace_central.js.php
+++ scripts/replace_central.js.php
@@ -19,25 +19,10 @@ var root_mydasboard_doc = "<?php echo PLUGIN_MYDASHBOARD_WEBDIR; ?>";
         // Start the plugin
         function init() {
             $(document).ready(function () {
-                if(check_path[2] == 'plugins') {
-                    var path = 'plugins/mydashboard/';
-                    var url = window.location.href.replace(/front\/.*/, path);
-                    if (window.location.href.indexOf('plugins') > 0) {
-                        url = window.location.href.replace(/plugins\/.*/, path);
-                    }
-                }else {
-                    var path = 'marketplace/mydashboard/';
-                    var url = window.location.href.replace(/front\/.*/, path);
-                    if (window.location.href.indexOf('marketplace') > 0) {
-                        url = window.location.href.replace(/marketplace\/.*/, path);
-                    }
-                }
 
-                var hrefs = $("a[href$='/front/central.php']");//, a[href$='/front/helpdesk.public.php']
+                var hrefs = $("a[href$='/front/central.php']");
                 hrefs.each(function (href, value) {
-                    if (value['pathname'].indexOf('plugins') < 0) {
-                        $("a[href='" + value['pathname'] + "']").attr('href', url + 'front/menu.php');
-                    }
+                    $("a[href='" + value['pathname'] + "']").attr('href', root_mydasboard_doc + '/front/menu.php');
                 });
             });
         }
