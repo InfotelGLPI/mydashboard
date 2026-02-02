6dbf8812a7450ccc4514992d8c151a112dbbea53
Thetsmr <thetsmr@gmail.com>
2026-02-02 09:13:07 +0100

    Bump version 2.2.4

--- setup.php
+++ setup.php
@@ -25,7 +25,7 @@
  --------------------------------------------------------------------------
  */
 
-define('PLUGIN_MYDASHBOARD_VERSION', '2.2.3');
+define('PLUGIN_MYDASHBOARD_VERSION', '2.2.4');
 
 global $CFG_GLPI;
 
