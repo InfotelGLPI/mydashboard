264e7e2f0c7e863a7d38c72fe6e1bc6d772a2db2
Thetsmr <thetsmr@gmail.com>
2026-05-07 10:44:52 +0200

    Fix test

--- tests/WidgetloadTest.php
+++ tests/WidgetloadTest.php
@@ -14,7 +14,7 @@ use GlpiPlugin\Mydashboard\Widget;
  */
 class WidgetloadTest extends DbTestCase
 {
-    protected function setUp(): void
+    public function setUp(): void
     {
         parent::setUp();
         $this->login('glpi', 'glpi');
